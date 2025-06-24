43425690b8dfadd0fe8dd677b578e24a58cdbf9f
Miguel Ticaray <miguelticaray@gmail.com>
2025-06-24 19:50:26 -0400

    js-initial

---
+++ JavaScript/seccion1.js
@@ -0,0 +1,51 @@
+//Esto es un comentario de una línea
+/*
+  Comentario de multiples
+  líneas
+*/
+
+//Variables
+/*
+  Las variables son espacios de memoria que almacenan un valor.
+*/
+// Declaración de una variable
+let nombre = "Juan"; // Variable
+console.log(nombre); // Imprime "Juan"
+// Reasignación de una variable
+nombre = "Pedro"; // Reasignación
+console.log(nombre); // Imprime "Pedro"
+
+// Tipos de datos
+let number = 42; // Número
+let float = 3.14; // Número de punto flotante
+let string = "Hola, mundo"; // Cadena de texto
+let boolean = true; // Booleano (true/false)
+let array = ["pera", "manzana", "uva"]; // Array (lista)
+console.log(array); // Imprime ["pera", "manzana", "uva"]
+console.log(array[0]); // Imprime "pera" (primer elemento del array)
+let nulo = null; // Nulo (sin valor)
+let indefinido; // Indefinido (sin asignar)
+let objeto = { nombre: "Juan", edad: 30 }; // Objeto
+console.log(objeto); // Imprime { nombre: "Juan", edad: 30 }
+console.log(objeto.nombre); // Imprime "Juan" (acceso a una propiedad del objeto)
+
+
+let suma_numeros = 5 + 3; // Suma
+let suma_string = "a10232!.;--''''-" + "2"; // Resta ()
+
+//Practica
+let prueba = "Pedro";
+prueba = 23;
+prueba = [1,2,3];
+console.log(prueba);
+
+console.log(prueba[1]) //imprime 2
+console.log(2 + 2);
+
+let estudiante = {
+  nombre: "José",
+  apellido: "Changir",
+  edad: 17
+}
+
+console.log(estudiante.edad);
