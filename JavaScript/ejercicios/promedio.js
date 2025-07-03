1e6047776d55c3e528f50386294885b6abf3a8ea
Miguel Ticaray <142277133+METF98@users.noreply.github.com>
2025-07-03 17:00:02 -0400

    create-promedio

---
+++ JavaScript/ejercicios/promedio.js
@@ -0,0 +1,14 @@
+function promedio(notas=[], suma=0, contador=0) {
+        
+        if (numero !== 0) {
+            suma += numero;
+            contador++;
+        }
+
+    if (contador > 0) {
+        const promedio = suma / contador;
+        alert(`El promedio es: ${promedio}`);
+    } else {
+        alert("No se ingresaron números.");
+    }
+}
