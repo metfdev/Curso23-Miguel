ae4ef0b0a335c333b5024ee1209443211bd72e91
Miguel Ticaray <142277133+metfdev@users.noreply.github.com>
2025-09-09 16:51:46 -0400

    Add React button practice project with Vite setup
    Initial commit for the Boton_practica React project using Vite. Includes project configuration, ESLint setup, basic source files, and assets. Also adds .gitignore and README for project documentation.

---
+++ react/basic/Boton_practica/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hola este es mi Boton</h1>
+      <div className="card">
+        <button>
+          Click aqui
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
